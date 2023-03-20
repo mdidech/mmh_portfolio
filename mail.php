01113be02dd6d3964bf8c37d4d0791254dc6e410
abdelaziz <abdelaziz.mdidech@gmail.com>
2023-03-20 11:19:10 +0000

    fix script mail commit

--- mail.php
+++ mail.php
@@ -1,13 +1,21 @@
 <?php
 
 if (isset($_POST['submit'])) {
+ // Set SMTP server, port number, and authentication credentials
+ ini_set('SMTP', 'mail.mmarketinghelp.com');
+ ini_set('smtp_port', '465');
+ ini_set('smtp_auth', true);
+ ini_set('username', 'contact@mmarketinghelp.com');
+ ini_set('password', ',m_;fh()..H~');
+
+
  // Collect form data
  $name = $_POST['name'];
  $email = $_POST['email'];
  $message = $_POST['message'];
 
  // Set recipient email address
- $to = 'contact@mmarketinghelp.com';
+ $to = 'abdelaziz.mdidech@gmail.com';
 
  // Set email subject
  $subject = 'New Contact Form Submission';
